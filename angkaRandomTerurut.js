54eb6544cf5172d5e07e4a119c8fe5d95dec3030
abrarnaim015 <aabrarnaim24@gmail.com>
2020-09-27 00:29:48 +0700

    v-1

---
+++ angkaRandomTerurut.js
@@ -0,0 +1,91 @@
+
+
+
+let numPar = nilaiRandomArr(5)
+let strPar = numPar[0]
+
+function urutAngka(str, arr)
+{
+    let parStr = str.toLowerCase()
+    let parArr = arr.length
+    for(let i=0; i<parArr; i++)
+    {
+        for(let x=i+1; x<parArr; x++)
+        {
+            if(parStr==='b')
+            {
+                if(arr[i]<arr[x])
+                {
+                    let tamp = arr[i]
+                    arr[i] = arr[x]
+                    arr[x] = tamp
+                }
+            }
+            else if(parStr==='k')
+            {
+                if(arr[i]>arr[x])
+                {
+                    let tamp = arr[i]
+                    arr[i] = arr[x]
+                    arr[x] = tamp
+                }
+            }
+        }
+    }
+    // console.log(arr)
+    return arr
+}
+
+function nilaiRandomArr(angka)
+{
+    let output = []
+    let acuan = Math.floor(Math.random()*10)+1
+    if(acuan%2===0)
+    {
+        output.push('B')
+    }
+    else
+    {
+        output.push('K')
+    }
+    for(let i=0; i<angka; i++)
+    {
+        output.push(Math.floor(Math.random()*10)+1)
+    } 
+    return output
+}
+console.log(urutAngka(strPar, numPar))
+
+
+function piramid(num)
+{
+    let x = 'x'
+    let o = 'o'
+    let parameter = 0
+    let box = ''
+    for(let i=0; i<num; i++)
+    {
+        let output = ''
+        parameter++
+        for(let j=0; j<parameter; j++)
+        {
+            if(j%2===0)
+            {
+                output+=o
+            }
+            else
+            {
+                output+=x
+            }
+        }
+        console.log(output)
+    }
+}
+piramid(numPar[1])
+
+
+// console.log(nilaiRandomArr(8)[0])
+// console.log(urutAngka(strPar, numPar))
+// console.log(par, strPar)
+// console.log(nilaiRandomArr(9))
+// console.log(Math.floor(Math.random()*10)+1)
